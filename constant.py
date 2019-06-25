5d5ec26c7a37643b6776678321edca8cc9120f40
IndianaS <benjamin.rejaud@gmail.com>
2019-06-25 16:31:54 +0200

    Premiere variables

--- constant.py
+++ constant.py
@@ -1,2 +1,26 @@
 #!/usr/bin/python3.5
 # -*-coding:utf-8 -
+"""Place where constants are"""
+
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+#Dimensions of game window
+NB_SPRITE = 15
+SPRITE_SIZE = 30
+SCREEN_SIZE = ((NB_SPRITE + 2) * SPRITE_SIZE, (NB_SPRITE + 2) * SPRITE_SIZE)
+
+#Display video of window game
+window = pygame.display.set_mode(SCREEN_SIZE)
+TITLE_WINDOW = "help MacGyver!!"
+
+#Screen (home)
+HOME = pygame.image.load("#####/###").convert()
+
+#Screen (Welcome to the game)
+WELCOME = pygame.image.load("######/####").convert_alpha()
+
+#Screen (Game-Over)
+GAMEOVER = pygame.image.load("pictures/game_over.png").convert_alpha()
